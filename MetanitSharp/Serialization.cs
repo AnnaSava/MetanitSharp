c860d51f53085bc2e8370f1afb2b0770eb1034a7
Anna Savina <anna.s.savina@gmail.com>
2018-11-23 23:12:54 +0300

    Serialization: SOAP, XML, JSON

--- MetanitSharp/Serialization.cs
+++ MetanitSharp/Serialization.cs
@@ -24,6 +24,15 @@ namespace MetanitSharp
                     case 'b':
                         SerializationBinary.Display();
                         break;
+                    case 's':
+                        SerializationSoap.Display();
+                        break;
+                    case 'l':
+                        SerializationXml.Display();
+                        break;
+                    case 'j':
+                        SerializationJson.Display();
+                        break;
                     case 'x': return;
                 }
                 Console.ReadKey();
@@ -34,6 +43,9 @@ namespace MetanitSharp
         {
             Console.WriteLine("Нажмите клавишу для вывода информации");
             Console.WriteLine("B - бинарная сериализация");
+            Console.WriteLine("S - SOAP сериализация");
+            Console.WriteLine("L - XML сериализация");
+            Console.WriteLine("L - JSON сериализация");
             Console.WriteLine("X - выход из раздела");
         }
     }
--- MetanitSharp/SerializationClasses.cs
+++ MetanitSharp/SerializationClasses.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace MetanitSharp
 {
@@ -82,6 +86,294 @@ namespace MetanitSharp
             {
                 Name = name;
                 Age = age;
+            }
+        }
+    }
+
+    class SerializationSoap
+    {
+        public static void Display()
+        {
+            Person person = new Person("Tom", 29);
+            Person person2 = new Person("Bill", 25);
+            Person[] people = new Person[] { person, person2 };
+
+            // создаем объект SoapFormatter
+            SoapFormatter formatter = new SoapFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream(@"C:\Metanit\people.soap", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, people);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream(@"C:\Metanit\people.soap", FileMode.OpenOrCreate))
+            {
+                Person[] newPeople = (Person[])formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                foreach (Person p in newPeople)
+                {
+                    Console.WriteLine("Имя: {0} --- Возраст: {1}", p.Name, p.Age);
+                }
+            }
+        }
+
+        [Serializable]
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+    }
+
+    public class SerializationXml
+    {
+        public static void Display()
+        {
+            serializeSingle();
+            serializeArr();
+            serializeComplex();
+        }
+
+        static void serializeSingle()
+        {
+            // объект для сериализации
+            Person person = new Person("Tom", 29);
+            Console.WriteLine("Объект создан");
+
+            // передаем в конструктор тип класса
+            XmlSerializer formatter = new XmlSerializer(typeof(Person));
+
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream(@"C:\Metanit\person.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, person);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream(@"C:\Metanit\person.xml", FileMode.OpenOrCreate))
+            {
+                Person newPerson = (Person)formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine("Имя: {0} --- Возраст: {1}", newPerson.Name, newPerson.Age);
+            }
+        }
+
+        static void serializeArr()
+        {
+            Person person1 = new Person("Tom", 29);
+            Person person2 = new Person("Bill", 25);
+            Person[] people = new Person[] { person1, person2 };
+
+            XmlSerializer formatter = new XmlSerializer(typeof(Person[]));
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\people.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, people);
+            }
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\people.xml", FileMode.OpenOrCreate))
+            {
+                Person[] newpeople = (Person[])formatter.Deserialize(fs);
+
+                foreach (Person p in newpeople)
+                {
+                    Console.WriteLine("Имя: {0} --- Возраст: {1}", p.Name, p.Age);
+                }
+            }
+        }
+
+        static void serializeComplex()
+        {
+            Employee person1 = new Employee("Tom", 29, new Company("Microsoft"));
+            Employee person2 = new Employee("Bill", 25, new Company("Apple"));
+            Employee[] people = new Employee[] { person1, person2 };
+
+            XmlSerializer formatter = new XmlSerializer(typeof(Employee[]));
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\employees.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, people);
+            }
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\employees.xml", FileMode.OpenOrCreate))
+            {
+                Employee[] newpeople = (Employee[])formatter.Deserialize(fs);
+
+                foreach (Employee p in newpeople)
+                {
+                    Console.WriteLine("Имя: {0} --- Возраст: {1} --- Компания: {2}", p.Name, p.Age, p.Company.Name);
+                }
+            }
+        }
+
+        // класс и его члены объявлены как public
+        [Serializable]
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            // стандартный конструктор без параметров
+            public Person()
+            { }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        [Serializable]
+        public class Employee
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Company Company { get; set; }
+
+            public Employee()
+            { }
+
+            public Employee(string name, int age, Company comp)
+            {
+                Name = name;
+                Age = age;
+                Company = comp;
+            }
+        }
+
+        [Serializable]
+        public class Company
+        {
+            public string Name { get; set; }
+
+            // стандартный конструктор без параметров
+            public Company() { }
+
+            public Company(string name)
+            {
+                Name = name;
+            }
+        }
+    }
+
+    class SerializationJson
+    {
+        public static void Display()
+        {
+            serialize();
+            serializeComplex();
+        }
+
+        static void serialize()
+        {
+            // объект для сериализации
+            Person person1 = new Person("Tom", 29);
+            Person person2 = new Person("Bill", 25);
+            Person[] people = new Person[] { person1, person2 };
+
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Person[]));
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\people.json", FileMode.OpenOrCreate))
+            {
+                jsonFormatter.WriteObject(fs, people);
+            }
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\people.json", FileMode.OpenOrCreate))
+            {
+                Person[] newpeople = (Person[])jsonFormatter.ReadObject(fs);
+
+                foreach (Person p in newpeople)
+                {
+                    Console.WriteLine("Имя: {0} --- Возраст: {1}", p.Name, p.Age);
+                }
+            }
+        }
+
+        static void serializeComplex()
+        {
+            Employee person1 = new Employee("Tom", 29, new Company("Microsoft"));
+            Employee person2 = new Employee("Bill", 25, new Company("Apple"));
+            Employee[] people = new Employee[] { person1, person2 };
+
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Employee[]));
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\employees.json", FileMode.OpenOrCreate))
+            {
+                jsonFormatter.WriteObject(fs, people);
+            }
+
+            using (FileStream fs = new FileStream(@"C:\Metanit\employees.json", FileMode.OpenOrCreate))
+            {
+                Employee[] newpeople = (Employee[])jsonFormatter.ReadObject(fs);
+
+                foreach (Employee p in newpeople)
+                {
+                    Console.WriteLine("Имя: {0} --- Возраст: {1} --- Компания: {2}", p.Name, p.Age, p.Company.Name);
+                }
+            }
+        }
+
+        [DataContract]
+        public class Person
+        {
+            [DataMember]
+            public string Name { get; set; }
+            [DataMember]
+            public int Age { get; set; }
+
+            public Person(string name, int year)
+            {
+                Name = name;
+                Age = year;
+            }
+        }
+
+        [DataContract]
+        public class Employee
+        {
+            [DataMember]
+            public string Name { get; set; }
+            [DataMember]
+            public int Age { get; set; }
+            [DataMember]
+            public Company Company { get; set; }
+
+            public Employee()
+            { }
+
+            public Employee(string name, int age, Company comp)
+            {
+                Name = name;
+                Age = age;
+                Company = comp;
+            }
+        }
+
+        [DataContract]
+        public class Company
+        {
+            [DataMember]
+            public string Name { get; set; }
+
+            public Company() { }
+
+            public Company(string name)
+            {
+                Name = name;
             }
         }
     }
