1f57b0edbe14e4e27c84e36b595d718b36f22b14
Anna Savina <anna.s.savina@gmail.com>
2018-12-01 17:38:27 +0300

    Pointers

--- MetanitSharp/Memory.cs
+++ MetanitSharp/Memory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MetanitSharp
 {
-   public static class Memory
+    public static class Memory
     {
         public static void Run()
         {
@@ -30,6 +30,12 @@ namespace MetanitSharp
                     case 'c':
                         usingCombinedTemplate();
                         break;
+                    case 'u':
+                        usingUnsafe();
+                        pointerOperations();
+                        pointerOperations2();
+                        pointerToPointer();
+                        break;
                     case 'x': return;
                 }
                 Console.ReadKey();
@@ -42,6 +48,7 @@ namespace MetanitSharp
             Console.WriteLine("G - сборщик мусора");
             Console.WriteLine("F - финализируемые объекты");
             Console.WriteLine("C - комбинирование Dispose и Finalize");
+            Console.WriteLine("U - небезопасный код");
             Console.WriteLine("X - выход из раздела");
         }
 
@@ -111,7 +118,7 @@ namespace MetanitSharp
 
         static void Test()
         {
-            Person p = new Person { Name = "Tom" };            
+            Person p = new Person { Name = "Tom" };
         }
 
         static void tryDispose()
@@ -203,6 +210,103 @@ namespace MetanitSharp
             {
                 Console.WriteLine($"Объект {Number}: \tВызван деструктор");
                 Dispose(false);
+            }
+        }
+
+        static void usingUnsafe()
+        {
+            unsafe
+            {
+                int* x; // определение указателя
+                int y = 10; // определяем переменную
+
+                x = &y; // указатель x теперь указывает на адрес переменной y
+                Console.WriteLine(*x); // 10
+
+                y = y + 20;
+                Console.WriteLine(*x);// 30
+
+                *x = 50;
+                Console.WriteLine(y); // переменная y=50                
+            }
+        }
+
+        static void pointerOperations()
+        {
+            unsafe
+            {
+                int* x; // определение указателя
+                int y = 10; // определяем переменную
+                x = &y; // указатель x теперь указывает на адрес переменной y
+
+                // получим адрес переменной y
+                uint addr = (uint)x;
+                Console.WriteLine("Адрес переменной y: {0}", addr);
+
+                byte* bytePointer = (byte*)(addr + 4); // получить указатель на следующий байт после addr
+                Console.WriteLine("Значение byte по адресу {0}: {1}", addr + 4, *bytePointer);
+
+                // обратная операция
+                uint oldAddr = (uint)bytePointer - 4; // вычитаем четыре байта, так как bytePointer - указатель на байт
+                int* intPointer = (int*)oldAddr;
+                Console.WriteLine("Значение int по адресу {0}: {1}", oldAddr, *intPointer);
+
+                // преобразование в тип double
+                double* doublePointer = (double*)(addr + 4);
+                Console.WriteLine("Значение double по адресу {0}: {1}", addr + 4, *doublePointer);
+
+                float* floatPointer = (float*)addr;
+                Console.WriteLine("Значение float по адресу {0}: {1}", addr, *floatPointer);
+            }
+        }
+
+        static void pointerOperations2()
+        {
+            unsafe
+            {
+                char* charPointer = (char*)123000;
+                *charPointer = 'A';
+                charPointer += 4; // 123008
+                Console.WriteLine("Адрес {0}", (uint)charPointer);
+                *charPointer = 'B';
+
+                double* doublePointer = (double*)123000;
+                doublePointer = doublePointer + 3; // 123024
+                Console.WriteLine("Адрес {0}", (uint)doublePointer);
+
+                *doublePointer = 20;
+                Console.WriteLine("Значение double по адресу {0}: {1}", (uint)doublePointer, *doublePointer);
+
+                uint addr = (uint)charPointer - 8;
+                int* intPointer = (int*)addr;
+
+                Console.WriteLine("Адрес\tint");
+
+                for (int i = 1; i < 9; i++)
+                {
+                    Console.WriteLine("{0}\t{1}",
+                        (uint)intPointer, *intPointer);
+                    intPointer += 1;
+                }
+            }
+        }
+
+        static void pointerToPointer()
+        {
+            unsafe
+            {
+                int* x; // определение указателя
+                int y = 10; // определяем переменную
+
+                x = &y; // указатель x теперь указывает на адрес переменной y
+                int** z = &x; // указатель z теперь указывает на адрес, который указывает на указатель x
+                **z = **z + 40; // изменение указателя z повлечет изменение переменной y
+                Console.WriteLine($"y={y}"); // переменная y=50
+                Console.WriteLine($"**z={**z}"); // переменная **z=50
+                Console.WriteLine($"*x={*x}"); // переменная *x=50
+
+                Console.WriteLine("Адрес переменной y={0}", (uint)x);
+                Console.WriteLine("Адрес указателя x={0}", (uint)z);
             }
         }
     }
